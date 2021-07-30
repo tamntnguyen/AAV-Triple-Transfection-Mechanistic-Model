9d3e75699b01321c76ef4e1d1d8b4dca1e43f9a9
tamntnguyen <87383041+tamntnguyen@users.noreply.github.com>
2021-07-30 13:11:38 -0400

    Delete Model directory

--- Model/calc_growth_rate.m
+++
@@ -1,5 +0,0 @@
-function [mu,c] = calc_growth_rate(t)
-%% Cell growth fitted to cell growth curve as a function of time
-g   = [49427.9581111241, 0.0112812472592155];
-c   = 1e6 + g(1)/g(2)*(1 - exp(-g(2).*t));
-mu  = g(1)*exp(-g(2).*t)./c;
